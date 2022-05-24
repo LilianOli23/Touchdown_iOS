e60bb6ba7038b07460a36e86d3e88849d2676ff2
liliandeoliveirasilva <lilian.agricola@gmail.com>
2022-05-24 18:11:50 -0300

    Brands grid view

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,15 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Lilian De Oliveira Silva on 24/05/22.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    
+    let id: Int
+    let image: String
+    
+}
