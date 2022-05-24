734e63aa616143c669c24b57e5d46fdd94953391
liliandeoliveirasilva <lilian.agricola@gmail.com>
2022-05-24 15:20:28 -0300

    Prototype Adding Footerview

---
+++ Touchdown/App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Lilian De Oliveira Silva on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+
+    
+    
+    // MARK: - BODY
+    var body: some View {
+        FooterView()
+            .padding(.horizontal)
+    }
+}
+// MARK: - PREVIEW
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,29 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Lilian De Oliveira Silva on 24/05/22.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+// UX
+
+// API
+
+// IMAGE
+
+// FONT
+
+// STRING
+
+// MISC
