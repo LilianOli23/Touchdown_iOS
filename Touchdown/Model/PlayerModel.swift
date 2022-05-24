b174002f1677d9552ef5a2f9111df889cfecf01b
liliandeoliveirasilva <lilian.agricola@gmail.com>
2022-05-24 16:55:37 -0300

    Adding a carousel with Page Tab View

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Lilian De Oliveira Silva on 24/05/22.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    
+    let id: Int
+    let image: String
+    
+}
