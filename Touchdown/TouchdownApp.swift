76e6341bd8a546badaa71dd1f83a620fa5d3361d
liliandeoliveirasilva <lilian.agricola@gmail.com>
2022-05-24 14:54:30 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Lilian De Oliveira Silva on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
